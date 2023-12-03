743417bc29760a41be318256f10b6c02ebffc7e7
manasadepu <112828012+manasadepu@users.noreply.github.com>
2023-12-03 18:03:06 -0500

    added bfs and dfs

--- wiki_speedia_backend/AdjacencyList.h
+++ wiki_speedia_backend/AdjacencyList.h
@@ -5,6 +5,10 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stack>
+#include <set>
+#include <queue>
+
 using namespace std;
 
 class Category {
@@ -43,4 +47,51 @@ public:
             }
         }
     }
+
+    int countNodesDFS(const string& start, const string& end){
+        set<string> visited;
+        return dfs(start, end, visited, 0) - 1;
+    }
+
+    int dfs(const string& node, const string& end, set<string>& visited, int count) {
+        visited.insert(node);
+        if (node == end) return count;
+
+        for (const auto& adjNode : this->list[node]) {
+            if (visited.find(adjNode) == visited.end()) {
+                int result = dfs(adjNode, end, visited, count + 1);
+                if (result != -1) return result;
+            }
+        }
+        return -1; // Path not found
+    }
+
+    int countNodesBFS(const string& start, const string& end) {
+        if (start == end) return 0;
+
+        queue<pair<string, int>> q; // Pair of node and depth
+        set<string> visited;
+
+        // Initialize
+        q.push({start, 0});
+        visited.insert(start);
+
+        while (!q.empty()) {
+            auto [node, depth] = q.front();
+            q.pop();
+
+            for (const auto& adjNode : this->list[node]) {
+                if (visited.find(adjNode) == visited.end()) {
+                    visited.insert(adjNode);
+                    q.push({adjNode, depth + 1});
+
+                    if (adjNode == end) return depth;
+                }
+            }
+        }
+        return -1; // If end node is not reachable from start node
+    }
+
+
+
 };
---
+++ wiki_speedia_backend/Algorithms.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+class AdjacencyList{
+private:
+
+map<string, string> pageNames;
+map<string, vector<string>> list;
+
+public:
+void DFS(string start, string finish){
+    
+}
+
+};
