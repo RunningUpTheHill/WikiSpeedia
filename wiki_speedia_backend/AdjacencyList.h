a428a785fbd88b1ae6fd0406972711af618eefd8
manasadepu <112828012+manasadepu@users.noreply.github.com>
2023-12-04 21:57:13 -0500

    Added more API calls

--- wiki_speedia_backend/AdjacencyList.h
+++ wiki_speedia_backend/AdjacencyList.h
@@ -8,28 +8,95 @@
 #include <stack>
 #include <unordered_set>
 #include <queue>
+#include "crow_all.h"
 
 using namespace std;
 
-class Category {
-    string CategoryName;
-    vector<string> pages;
-public:
-    explicit Category(string name) {
-        CategoryName = std::move(name);
-    };
-    void append(const string& page) {
-        pages.push_back(page);
-    };
-};
+string urlDecode(const std::string& str) {
+    std::string decoded;
+    char ch;
+    int i, j;
+    for (i = 0; i < str.length(); i++) {
+        if (int(str[i]) == 37) { // '%' in ASCII
+            sscanf(str.substr(i + 1, 2).c_str(), "%x", &j);
+            ch = static_cast<char>(j);
+            decoded += ch;
+            i = i + 2;
+        } else {
+            decoded += str[i];
+        }
+    }
+    return decoded;
+}
+
+//class Category {
+//public:
+//    string CategoryName;
+//    unordered_map<string, string> pages;
+//
+//    explicit Category(string name) {
+//        CategoryName = std::move(name);
+//    };
+//    void append(const string& page) {
+//        pages[page] = CategoryName;
+//    };
+//};
 
 class AdjacencyList {
+
 public:
+    unordered_map<string, string> categories;
     unordered_map<string, string> pageNames;
     unordered_map<string, vector<string> > list;
+    crow::json::wvalue graph;
+
+    string getCategory(const string& node) {
+        return categories[node];
+    }
+
+    vector<string> getAllPageNames() {
+        vector<string> allNames;
+        int i = 0;
+        for (const auto& pair : pageNames) {
+            if (i > 10000){
+                break;
+            }
+            allNames.push_back(pair.second);
+            i++;
+        }
+        return allNames;
+    }
+
+    crow::json::wvalue getNodesAndEdges() {
+        crow::json::wvalue result;
+        int nodeId = 1;
+        unordered_map<string, int> nodeNameToId;
+
+        // Process nodes
+        for (const auto& pair : list) {
+            nodeNameToId[pair.first] = nodeId;
+            result["nodes"][nodeId - 1] = {{"id", nodeId}, {"label", pair.first}, {"shape", "circular"}};
+            nodeId++;
+        }
+
+        // Process edges
+        int edgeId = 0;
+        for (const auto& pair : list) {
+            for (const auto& adjNode : pair.second) {
+                result["edges"][edgeId] = {{"from", nodeNameToId[pair.first]}, {"to", nodeNameToId[adjNode]}};
+                edgeId++;
+            }
+        }
+
+        return result;
+    }
+
+
+
+
     AdjacencyList() {
         ifstream inFile;
-        inFile.open("wiki-topcats-page-names.txt");
+        inFile.open("/Users/manasadepu/CLionProjects/WikiSpeedia/wiki_speedia_backend/wiki-topcats-page-names.txt");
         string line;
         while (getline(inFile, line)) {
             stringstream s(line);
@@ -39,7 +106,7 @@ public:
             pageNames[key] = line.substr(it-line.begin()+1, line.size()-*it-1);
         }
         ifstream linkFile;
-        linkFile.open("wiki-topcats.txt");
+        linkFile.open("/Users/manasadepu/CLionProjects/WikiSpeedia/wiki_speedia_backend/wiki-topcats.txt");
         string props;
         while (getline(linkFile, props)) {
             stringstream ss(props);
@@ -50,6 +117,20 @@ public:
                 list[pageNames[to]] = {};
             }
         }
+        ifstream i;
+        string category;
+        i.open("/Users/manasadepu/CLionProjects/WikiSpeedia/wiki_speedia_backend/wiki-topcats-categories.txt");
+        while (getline(i, category)) {
+            stringstream s(category);
+            string placeholder;
+            s >> placeholder;
+            string CATE = placeholder.substr(9, placeholder.size()-1-9);
+            while (s >> placeholder) {
+                categories[pageNames[placeholder]] = CATE;
+            }
+        }
+
+        cout << "Finished initializing List";
     }
 
     int countNodesDFS(const string& start, const string& end) {
@@ -99,6 +180,53 @@ public:
                 if (adjNode == end) return node.second + 1;
             }
         }
+        return -1; // If end node is not reachable from start node
+    }
+
+    int aStarSearch(const string& start, const string& end) {
+        // Define the heuristic function
+        auto heuristic = [this, &end](const string& node) {
+            string nodeCategory = getCategory(node);
+            string targetCategory = getCategory(end);
+
+            if (nodeCategory == targetCategory) {
+                return 0; // Lower cost if node is in the same category as target
+            } else {
+                return 1; // Higher cost otherwise
+            }
+        };
+
+        // Priority queue for the frontier, sorted by f = g + h
+        auto cmp = [](const pair<string, int>& a, const pair<string, int>& b) {
+            return a.second > b.second;
+        };
+        priority_queue<pair<string, int>, vector<pair<string, int>>, decltype(cmp)> frontier(cmp);
+
+        unordered_map<string, int> cost_so_far; // Cost from start to a node
+        unordered_set<string> visited; // Visited nodes
+
+        frontier.emplace(start, 0);
+        cost_so_far[start] = 0;
+
+        while (!frontier.empty()) {
+            auto current = frontier.top().first;
+            frontier.pop();
+
+            if (current == end) {
+                return cost_so_far[current]; // Path found
+            }
+
+            for (const auto& next : list[current]) {
+                int new_cost = cost_so_far[current] + 1; // Assuming uniform cost for each step
+                if (!cost_so_far.count(next) || new_cost < cost_so_far[next]) {
+                    cost_so_far[next] = new_cost;
+                    int priority = new_cost + heuristic(next);
+                    frontier.emplace(next, priority);
+                    visited.insert(next);
+                }
+            }
+        }
+
         return -1; // If end node is not reachable from start node
     }
 };
--- wiki_speedia_backend/main.cpp
+++ wiki_speedia_backend/main.cpp
@@ -1,49 +1,83 @@
 #include "AdjacencyList.h"
-#include "crow_all.h"
+
+#include <chrono>
 using namespace std;
 
 int main() {
-
-    crow::SimpleApp app;
     AdjacencyList list;
+    crow::SimpleApp app;
 
-    ifstream inFile;
-    string category;
-    inFile.open("wiki-topcats-categories.txt");
-    vector<Category> categories;
-    while (getline(inFile, category)) {
-        stringstream s(category);
-        string placeholder;
-        s >> placeholder;
-        Category Cate(placeholder.substr(9, placeholder.size() - 1 - 9));
-        while (s >> placeholder) {
-            Cate.append(placeholder);
-        }
-        categories.push_back(Cate);
-    }
+    CROW_ROUTE(app, "/get_page_names")
+            ([&list]() {
+                auto pageNames = list.getAllPageNames(); // Call the method to get all page names
+                crow::json::wvalue result;
+                result["pageNames"] = pageNames;
+
+                crow::response res{result};
+                res.add_header("Access-Control-Allow-Origin", "*"); // Allow all origins
+                res.add_header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+                res.add_header("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
+
+                return res;
+            });
 
     CROW_ROUTE(app, "/search/<string>/<string>")
             ([&](const std::string& start, const std::string& end) {
 
-                int A = list.countNodesBFS(start, end);
-                int B = list.countNodesDFS(start, end);
+                string src = urlDecode(start);
+                string dest = urlDecode(end);
+
+                // Measure time for BFS
+                auto bfsStart = chrono::steady_clock::now();
+                int A = list.countNodesBFS(src, dest);
+                auto bfsEnd = chrono::steady_clock::now();
+                long long bfsDuration = chrono::duration_cast<chrono::nanoseconds>(bfsEnd - bfsStart).count();
+
+                // Measure time for DFS
+                auto dfsStart = chrono::steady_clock::now();
+                int B = list.countNodesDFS(src, dest);
+                auto dfsEnd = chrono::steady_clock::now();
+                long long dfsDuration = chrono::duration_cast<chrono::nanoseconds>(dfsEnd - dfsStart).count();
+
+                //Measure time for A*
+                auto astarStart = chrono::steady_clock::now();
+                int C = list.aStarSearch(src, dest);
+                auto astarEnd = chrono::steady_clock::now();
+                long long astarDuration = chrono::duration_cast<chrono::nanoseconds>(astarEnd - astarStart).count();
+
 
                 crow::json::wvalue result;
-                result["User Entered Values"] = start + " " + end;
+                result["User Entered Values"] = src + " " + dest;
                 if (A > -1){
                     result["message"] = "Success";
-                    result["node_count"] = A;
+                    result["BFS_node_count"] = A;
+                    result["BFS_time_ns"] = bfsDuration;
+                    result["DFS_node_count"] = B;
+                    result["DFS_time_ns"] = dfsDuration;
+                    result["A*_node_count"] = C;
+                    result["A*_time_ns"] = astarDuration;
                 } else {
                     result["message"] = "Path not found or error";
                 }
-                return result;
+                crow::response res{result};
+                res.add_header("Access-Control-Allow-Origin", "*"); // Allow all origins
+                res.add_header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+                res.add_header("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
+
+                return res;
+            });
+
+    CROW_ROUTE(app, "/get_graph_data")
+            ([&list]() {
+                auto graphData = list.getNodesAndEdges();
+                crow::response res{graphData};
+                res.add_header("Access-Control-Allow-Origin", "*");
+                res.add_header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+                res.add_header("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
+                return res;
             });
 
     app.port(18080).multithreaded().run();
-
-
-
-
 
     return 0;
 }
